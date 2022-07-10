7a302845cede28448f9ab93d946c66b4bfce10bb
Jason Noble <jason.noble@itron.com>
2022-07-10 13:41:57 -0600

    Iteration 1 complete

---
+++ lib/calculator.rb
@@ -0,0 +1,7 @@
+class Calculator
+  attr_reader :total
+
+  def initialize
+    @total = 0.00
+  end
+end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require './lib/calculator'
+
+RSpec.describe Calculator do
+  let(:calculator) { described_class.new }
+
+  describe '#new' do
+    it 'creates a Calculator' do
+      expect(calculator).to be_a(described_class)
+    end
+  end
+
+  describe '#total' do
+    it 'returns 0.00 by default' do
+      expect(calculator.total).to eq(0.00)
+    end
+  end
+end
